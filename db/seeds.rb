b1bdc3d8b08e88aac2b2b582e02206af84cc63ad
moritzbe <moritzberthold@hotmail.com>
2015-02-03 21:43:55 +0100

    added seeds and counters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Shorter.create(url:"fb" ,redirection: "http://www.facebook.com")
-Shorter.create(url:"eb" ,redirection: "http://www.ebay.de")
-Shorter.create(url:"git" ,redirection: "http://www.github.com")
-Shorter.create(url:"rails" ,redirection: "http://www.rubyonrails.org")
+Shorter.create(url:"fb" ,redirection: "http://www.facebook.com", counter: 3)
+Shorter.create(url:"eb" ,redirection: "http://www.ebay.de", counter: 0)
+Shorter.create(url:"git" ,redirection: "http://www.github.com", counter: 1)
+Shorter.create(url:"rails" ,redirection: "http://www.rubyonrails.org", counter: 4)
 

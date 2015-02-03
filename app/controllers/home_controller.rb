410174a7e2b913d928bf75d3707ea153a6bf95bd
moritzbe <moritzberthold@hotmail.com>
2015-02-03 22:06:03 +0100

    yeah, everything works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,19 @@ class HomeController < ApplicationController
 	#then shows views!
 	def home
 		if !Shorter.find_by(url: params[:url]).nil?
-			redir = Shorter.find_by(url: params[:url]).redirection
-			newrl=URI.parse(redir)
+			redir = Shorter.find_by(url: params[:url])
+			redir.counter +=1
+			redir.save
+			newrl=URI.parse(redir.redirection)
 			redirect_to newrl.to_s
-		else
-		@stats = Shorter.all
 		end	
+		@stats = Shorter.all
+#.limit(10).order("counters DESC")
 	end
+
+	def stats		
+		redirect("/most_visited_pages")
+	end	
 
 	def create
 		# entry = Shorter.create(url: params[:url])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #The router: Commands and talks to Controller
  get "/:url" => "home#home"
+ get "/" => "home#home"
  get "/most_visited_pages" =>"home#stats"
 
  # post "/create" => "home#create"
